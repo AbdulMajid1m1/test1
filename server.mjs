2d8282ac570f9d0cdc38ca3b69c167560dbccd80
unknown <abdulmajid1m2@gmail.com>
2024-03-23 04:52:50 +0500

    fixed vercel

--- server.mjs
+++ server.mjs
@@ -74,7 +74,7 @@ app.use((err, req, res, next) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     connect();
     console.log(`Server is running on port ${PORT}`);
