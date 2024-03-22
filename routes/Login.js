2f89b982e657e857d3f20e3e07eef522f8f03c22
unknown <abdulmajid1m2@gmail.com>
2024-03-23 04:59:26 +0500

    fixed vercels

--- routes/Login.js
+++ routes/Login.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { SignIn,signUp,logout,refreshAccessToken, get} from '../controllers/Login.js';
+import { SignIn, signUp, logout, refreshAccessToken, get } from '../controllers/Login.js';
 
 import { auth } from '../middleware/auth.js';
 const router = express.Router();
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/signUp', signUp);
 router.get('/get', get);
 router.post('/login', SignIn);
-router.post('/refresh-dtoken',auth, refreshAccessToken);
+router.post('/refresh-dtoken', auth, refreshAccessToken);
 router.post('/logout', logout);
 router.get("/", (req, res) => {
     res.send("API is running!");
